8ce29ffaff622315a754be22b092834686bb0cad
kaappoo <kaua.ka373@gmail.com>
2024-09-08 01:29:40 -0300

    fix: fix home path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CardsComponent } from './pages/cards/cards.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
+    // {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '', component: HomeComponent},
     {path: 'cards', component: CardsComponent},
 ];
