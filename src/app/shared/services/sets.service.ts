0aadfa6d05bbce6b4da2a3e8b74d90b2f901e056
kaappoo <kaua.ka373@gmail.com>
2024-09-11 12:10:41 -0300

    feat: create sets service

---
+++ src/app/shared/services/sets.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { SetResponse } from '../interfaces/set';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SetsService {
+
+  private http = inject(HttpClient);
+  private header = new HttpHeaders({'x-api-key': environment.api.pokemontcg.apiKey});
+
+  getSets(): Observable<SetResponse>{
+    return this.http.get<SetResponse>(`${environment.api.pokemontcg.uri}/sets`, {headers: this.header});
+  }
+}
