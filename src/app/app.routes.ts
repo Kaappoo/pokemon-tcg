ef2199968c6f43094f127db829f431124ff9d3a8
kaappoo <kaua.ka373@gmail.com>
2024-09-15 00:52:01 -0300

    feat: generate view card component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CardsComponent } from './pages/cards/cards.component';
+import { ViewCardComponent } from './pages/view-card/view-card.component';
 
 export const routes: Routes = [
     // {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '', component: HomeComponent},
     {path: 'cards', component: CardsComponent},
+    {path: 'view-card', component: ViewCardComponent},
 ];
