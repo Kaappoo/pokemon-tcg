095c9bd8bf31d0fe248083c16196ed761bd48798
kaappoo <kaua.ka373@gmail.com>
2024-09-19 12:48:12 -0300

    feat: add prices to card-view page

--- src/app/pages/view-card/view-card.component.ts
+++ src/app/pages/view-card/view-card.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CardsService } from '../../shared/services/cards.service';
 import { SingularCardResponse } from '../../shared/interfaces/card';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-view-card',
   standalone: true,
-  imports: [],
+  imports: [TableModule],
   templateUrl: './view-card.component.html',
   styleUrl: './view-card.component.scss'
 })
@@ -17,6 +18,12 @@ export class ViewCardComponent {
   cardId: any;
   card: any;
   isLoading = false;
+
+  pricesTCG: any[] = [];
+  pricesKeysTCG: any[] = [];
+  pricesCM: any[] = [];
+  pricesKeysCM: any[] = [];
+
   ngOnInit(){
     this.cardId = this.route.snapshot.paramMap.get('id');
     this.getCard();
@@ -27,6 +34,16 @@ export class ViewCardComponent {
     this.cardService.getCard(this.cardId).subscribe((res: SingularCardResponse) => {
       this.card = res.data;
       this.isLoading = false;
+      console.log(this.card);
+      this.getPrices();
     })
+  }
+
+  getPrices(){
+    this.pricesKeysTCG = Object.getOwnPropertyNames(this.card.tcgplayer.prices);
+    this.pricesTCG = Object.values(this.card.tcgplayer.prices);
+    
+    this.pricesKeysCM = Object.getOwnPropertyNames(this.card.cardmarket.prices);
+    this.pricesCM = Object.values(this.card.cardmarket.prices);
   }
 }
