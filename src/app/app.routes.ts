f73da6301e0877b85f5058e0c7992c55c277d908
kaappoo <kaua.ka373@gmail.com>
2024-09-24 14:06:38 -0300

    feat: add sets router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CardsComponent } from './pages/cards/cards.component';
 import { ViewCardComponent } from './pages/view-card/view-card.component';
+import { SetsComponent } from './pages/sets/sets.component';
 
 export const routes: Routes = [
     // {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '', component: HomeComponent},
     {path: 'cards', component: CardsComponent},
     {path: 'view-card', component: ViewCardComponent},
+    {path: 'sets', component: SetsComponent},
 ];
