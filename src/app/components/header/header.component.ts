7718f654fc31910aeba4aae2428ce8a0b0e1bf66
kaappoo <kaua.ka373@gmail.com>
2024-09-23 21:32:29 -0300

    feat: add go to new releases function to header nav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { SetsService } from '../../shared/services/sets.service';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +11,12 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private setsService = inject(SetsService);
+  private router = inject(Router);
 
+  goToNewReleases(){
+    this.setsService.getNewestSet().subscribe((res: any) => {
+      this.router.navigate(['cards', {search: 'set', id: res.data[0].id}])
+    })
+  }
 }
